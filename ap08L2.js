7c450cad76dbb71bfbebec8f4876d1fc01bfef15
G484832024 <248483@st.takushoku-u.ac.jp>
2025-11-19 15:32:37 +0900

    L2 changed

--- ap08L2.js
+++ ap08L2.js
@@ -19,8 +19,10 @@ let course;
 export const origin = new THREE.Vector3();
 export const controlPoints = [
     [ 50,-20],
-    [  5,  0],
-    [-20, 20],
+    [  0,  0],
+    [  0, 60],
+    [-40, 70],
+    [-35, 30],
     [-25, 40]
 ]
 export function init(scene, size, id, offset, texture) {
@@ -48,17 +50,16 @@ export function init(scene, size, id, offset, texture) {
     scene.add(plane);
 
     // ビル
-    // function makeBuilding(x, z, type) {
-    //     const height = [2, 2, 7, 4, 5];
-    //     const bldgH = height[type]*5;
-    //     const geometry = new THREE.BoxGeometry(8, bldgH, 8);
-    //     const material = new THREE.MeshLambertMaterial({color: 0x808080}); //{map: texture}
-    /*
+    function makeBuilding(x, z, type) {
+        const height = [2, 2, 7, 4, 5];
+        const bldgH = height[type]*5;
+        const geometry = new THREE.BoxGeometry(8, bldgH, 8);
+        const material = new THREE.MeshLambertMaterial({map: texture});
         const sideUvS = (type*2+1)/11;
         const sideUvE = (type*2+2)/11;
         const topUvS = (type*2+2)/11;
         const topUvE = (type*2+3)/11;
-        const uvs = geometry.getAttibute("uv");
+        const uvs = geometry.getAttribute("uv");
         for (let i = 0; i < 48; i+=4) {
             if(i < 16 || i > 22) {
                 uvs.array[i] = sideUvS;
@@ -69,15 +70,15 @@ export function init(scene, size, id, offset, texture) {
                 uvs.array[i+2] = topUvE;
             }
         }
-    */
-    //     const bldg = new THREE.Mesh(
-    //         geometry,
-    //         material
-    //     )
-    //     bldg.position.set(1, 10, 1)
-    //     scene.add(bldg);
-    // }
-    // makeBuilding(20, 20, 0);
+        const bldg = new THREE.Mesh(
+            geometry,
+            material
+        )
+        bldg.position.set(x, bldgH, type)
+        scene.add(bldg);
+    }
+    makeBuilding(20, 20, 0);
+    makeBuilding(-65, 35, 0);
 
     // コース(描画)
     course = new THREE.CatmullRomCurve3 (
@@ -110,23 +111,23 @@ export function init(scene, size, id, offset, texture) {
 
 // コース(自動運転用)
 export function makeCourse(scene) {
-    // const courseVectors = [];
-    // const parts = [L2, L3, L4, L1];
-    // parts.forEach((part) => {
-    //     part.controlPoints.forEach((p) => {
-    //         courseVectors.push(
-    //             new THREE.Vector3(
-    //                 p[0] + part.origin.x,
-    //                 0,
-    //                 p[1] + part.origin.z,
-    //             )
-    //         )
-    //     });
-    // })
+    const courseVectors = [];
+    const parts = [L2, L3, L4, L1];
+    parts.forEach((part) => {
+        part.controlPoints.forEach((p) => {
+            courseVectors.push(
+                new THREE.Vector3(
+                    p[0] + part.origin.x,
+                    0,
+                    p[1] + part.origin.z,
+                )
+            )
+        });
+    })
+    course = new THREE.CatmullRomCurve3(
+        courseVectors, true
+    )
 }
-// course = new THREE.CatmullRomCurve3(
-//     courseVectors, true
-// )
 
 // カメラを返す
 export function getCamera() {
@@ -149,15 +150,15 @@ export function resize() {
 }
 
 // 描画処理
-// const clock = new THREE.Clock();
-// const carPosition = new THREE.Vector3();
-// const carTarget = new THREE.Vector3();
+const clock = new THREE.Clock();
+const carPosition = new THREE.Vector3();
+const carTarget = new THREE.Vector3();
 export function render(scene, car) {
-    // const time = (clock.getElapsedTime() / 20);
-    // course.getPointAt(time % 1, carPosition);
-    // car.position.copy(carPosition);
-    // course.getPointAt((time + 0.01) %1, carTarget);
-    // car.lookAt(carTarget);
+    const time = (clock.getElapsedTime() / 20);
+    course.getPointAt(time % 1, carPosition);
+    car.position.copy(carPosition);
+    course.getPointAt((time + 0.01) %1, carTarget);
+    car.lookAt(carTarget);
     camera.lookAt(car.position.x, car.position.y, car.position.z);
     renderer.render(scene, camera);
 }
