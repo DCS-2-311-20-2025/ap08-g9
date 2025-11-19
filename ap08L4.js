86b7640f6e93c68fa5a7456c72556c6f54390b77
G484892024 <248489@st.takushoku-u.ac.jp>
2025-11-19 15:36:43 +0900

    kuruma

--- ap08L4.js
+++ ap08L4.js
@@ -80,6 +80,22 @@ points.forEach((point) => {
 
 // コース(自動運転用)
 export function makeCourse(scene) {
+    const courseVectors = [];
+    const parts = [L4, L1, L2, L3];
+    parts.forEach((part) => {
+        part.controlPoints.forEach((p) => {
+            courseVectors.push(
+                new THREE.Vector3(
+                    p[0] + part.origin.x,
+                    0,
+                    p[1] + part.origin.z,
+                )
+            )
+        });
+    })
+    course = new THREE.CatmullRomCurve3(
+        courseVectors, true
+    )
 }
 
 // カメラを返す
@@ -105,7 +121,13 @@ export function resize() {
 // 描画処理
 const clock = new THREE.Clock();
 const carPosition = new THREE.Vector3();
+const carTarget = new THREE.Vector3();
 export function render(scene, car) {
+    const time = (clock.getElapsedTime() / 20);
+    course.getPointAt(time % 1, carPosition);
+    car.position.copy(carPosition);
+    course.getPointAt((time + 0.01) %1, carTarget);
+    car.lookAt(carTarget);
     camera.lookAt(car.position.x, car.position.y, car.position.z);
     renderer.render(scene, camera);
 }
